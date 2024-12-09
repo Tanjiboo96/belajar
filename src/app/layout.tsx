ef5da1d3bec90cd9362661821b932b1f8db016e0
Tanjiboo96 <leonardprakoso@gmail.com>
2024-12-09 16:03:19 +0700

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import localFont from "next/font/local";
+import "./globals.css";
+import "tailwindcss/base";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+      <nav>
+        <h1>Resep Pak Bambang</h1>
+      </nav>
+      {children}
+      </body>
+    </html>
+  );
+}
